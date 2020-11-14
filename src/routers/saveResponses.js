b88d8e1601b6d991ffc0f1a87e80f383fa33ec1e
RitadeCassiaFontenele <69485548+RitadeCassiaFontenele@users.noreply.github.com>
2020-11-14 12:22:14 -0300

    Update saveResponses.js

--- src/routers/saveResponses.js
+++ src/routers/saveResponses.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const Responses = require('../database/models/Responses');
+const Questions = require('../database/models/Questions');
 
-router.post("/responses", (req, res) => {
-	var body = req.body.response;
-	var id = req.body.responseId;
-
+router.post("/response/:id", (req, res) => {
+	var body = req.body.resp;
+	var id = req.params.id;
+	
 	Responses.create({
 		body: body,
 		questionId: id
 	}).then(()=> {
-		res.status(201).send("Resposta criada com sucesso");
+		res.status(200).send({
+			Mensagem: "Resposta criada com sucesso"
+		})
 	}).catch((error)=> {
-		res.status(500).send(error);
+		res.status(500).send({
+			error: error
+		})
 	})
-})
-
+});	
 
 module.exports = router;
